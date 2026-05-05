b3e33971adac9e155d2b566d043d909985221256
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2026-05-04 20:34:47 -0400

    fix cors 2034

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import serverless from "serverless-http";
-import app from "./src/app.js";
-import { connectDB } from "./src/db.js";
+import app from "../src/app.js";
+import { connectDB } from "../src/db.js";
 
-// conectar DB (esto sí se puede)
-connectDB();
+// ⚠️ importante: manejar conexión solo una vez
+let isConnected = false;
 
-export default serverless(app);
+async function connect() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+}
+
+export default async function handler(req, res) {
+  await connect();
+  return serverless(app)(req, res);
+}
